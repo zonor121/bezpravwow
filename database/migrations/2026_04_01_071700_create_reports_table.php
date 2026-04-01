8f742e3996bda4c7a2619237b65aa10b9aaee9b6
zonor121 <kopinator9000@gmail.com>
2026-04-01 12:34:59 +0500

    first commit

---
+++ database/migrations/2026_04_01_071700_create_reports_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->text('description');
+            $table->timestamps();
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->nullonDelete()
+                ->cascadeOnUpdate();
+            $table->foreignId('status_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
